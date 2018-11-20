085cbb17915dcb07359fe0acab4258cbe29b92e2
anhdung1803 <anhdung.phc@gmail.com>
2018-11-20 16:38:25 +0700

    Update code using gonic lib

--- main.go
+++ main.go
@@ -1,87 +1,107 @@
 package main
 
 import (
-	"encoding/json"
+	"flag"
 	"log"
 	"net/http"
+	das "todoAPI/da"
+	"todoAPI/models"
 
-	. "todoAPI/da"
-	. "todoAPI/models"
-
-	"github.com/gorilla/mux"
-	"gopkg.in/mgo.v2/bson"
+	"github.com/facebookgo/flagenv"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
-var da = ToDoDataAccess{}
+var da = das.ToDoDataAccess{}
+var mgoURI string
+var databaseName string
 
-func init() {
-	da.Server = "localhost"
-	da.Database = "todo"
+func int() {
+
+	godotenv.Load()
+
+	flag.StringVar(&mgoURI, "mgo-uri", "localhost", "uri to connect to mongodb")
+	flag.StringVar(&databaseName, "database-name", "todolist123", "database use to work")
+	flag.Parse()
+
+	flagenv.Parse()
+
+	da.Host = mgoURI
+	da.Database = databaseName
 	da.Connect()
 }
 
 // our main function
 func main() {
-	router := mux.NewRouter()
+	router := gin.Default()
 
-	router.HandleFunc("/todo", GetAll).Methods("GET")
-	router.HandleFunc("/todo", CreateTodoEndPoint).Methods("POST")
-	router.HandleFunc("/todo", DeleteTodoEndPost).Methods("DELETE")
-	if err := http.ListenAndServe(":8000", router); err != nil {
+	router.GET("/", GetAll)
+	router.POST("/createTodo", CreateTodoEndPoint)
+	router.PUT("/updateTodo", UpdateTodoEndPoint)
+	router.DELETE("/deleteTodo", DeleteTodoEndPost)
+	if err := router.Run(":8080"); err != nil {
 		log.Fatal(err)
 	}
 
 }
 
 // GetAll use to gell all Todo data
-func GetAll(w http.ResponseWriter, r *http.Request) {
+func GetAll(c *gin.Context) {
 	todo, err := da.FindAll()
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error())
+		c.String(http.StatusInternalServerError, err.Error())
 		return
 	}
-	respondWithJson(w, http.StatusOK, todo)
+	c.JSON(http.StatusOK, todo)
 }
 
-// CreateTodoEndPoint use to create task by id
-func CreateTodoEndPoint(w http.ResponseWriter, r *http.Request) {
-	defer r.Body.Close()
-	var todo Todo
-	if err := json.NewDecoder(r.Body).Decode(&todo); err != nil {
-		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+// CreateTodoEndPoint use to create task
+func CreateTodoEndPoint(c *gin.Context) {
+	var todo models.Todo
+	if err := c.ShouldBindJSON(&todo); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	todo.ID = bson.NewObjectId()
+
 	if err := da.Insert(todo); err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error())
+		c.JSON(http.StatusInternalServerError, err.Error())
 		return
 	}
-	respondWithJson(w, http.StatusCreated, todo)
+	c.JSON(http.StatusOK, todo)
+
+}
+
+// UpdateTodoEndPoint use to update data
+func UpdateTodoEndPoint(c *gin.Context) {
+
+	var todo models.Todo
+
+	if err := c.ShouldBindJSON(&todo); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "err.Error()"})
+		return
+	}
+
+	if err := da.Update(todo); err != nil {
+		c.JSON(http.StatusNotAcceptable, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, todo)
+
 }
 
 // DeleteTodoEndPost use to remove task by id
-func DeleteTodoEndPost(w http.ResponseWriter, r *http.Request) {
-	defer r.Body.Close()
-	var todo Todo
-	if err := json.NewDecoder(r.Body).Decode(&todo); err != nil {
-		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+func DeleteTodoEndPost(c *gin.Context) {
+
+	var todo models.Todo
+
+	if err := c.ShouldBindJSON(&todo); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
+
 	if err := da.Delete(todo); err != nil {
-		respondWithError(w, http.StatusBadRequest, err.Error())
+		c.JSON(http.StatusNotAcceptable, gin.H{"error": err.Error()})
 		return
 	}
-	respondWithJson(w, http.StatusOK, map[string]string{"result": "success"})
-}
-
-func respondWithError(w http.ResponseWriter, code int, msg string) {
-	respondWithJson(w, code, map[string]string{"error": msg})
-}
-
-// respondWithJson return data type json
-func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
-	response, _ := json.Marshal(payload)
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(code)
-	w.Write(response)
+	c.String(http.StatusOK, "Remove success todo with id %s", todo.ID)
 }
